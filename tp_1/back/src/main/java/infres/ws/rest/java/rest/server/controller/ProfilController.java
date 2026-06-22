2edaedf0f07c382a56b212307e46d1868f4b1df4
Cessouille <celian.chau@gmail.com>
2026-06-22 14:19:28 +0200

    fix: remove unused import

--- tp_1/back/src/main/java/infres/ws/rest/java/rest/server/controller/ProfilController.java
+++ tp_1/back/src/main/java/infres/ws/rest/java/rest/server/controller/ProfilController.java
@@ -2,7 +2,6 @@ package infres.ws.rest.java.rest.server.controller;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
