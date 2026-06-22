6a4d39585aa886405a4f1b19ddc5b294d1a55ca1
Cessouille <celian.chau@gmail.com>
2026-06-22 12:01:50 +0200

    feat: disable cors

---
+++ tp_1/back/src/main/java/infres/ws/rest/java/rest/server/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package infres.ws.rest.java.rest.server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) {
+        http.cors(cors -> cors.configurationSource(disableCorsConfigurationSource()));
+
+        return http.build();
+    }
+
+    private CorsConfigurationSource disableCorsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(List.of("*"));
+        configuration.setAllowedHeaders(List.of("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
