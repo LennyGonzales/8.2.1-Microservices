16f1bbbb59e8e85772da957957b47ad114971c49
Luca Ceccarelli <lucabuco03@gmail.com>
2026-06-23 15:45:24 +0200

    fix: Now kube works

--- tp_1/back/src/main/java/infres/ws/rest/java/rest/server/config/SecurityConfig.java
+++ tp_1/back/src/main/java/infres/ws/rest/java/rest/server/config/SecurityConfig.java
@@ -42,7 +42,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of(
+            "http://localhost:3000",
+            "http://front.infres.fr.localhost"
+        ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(false);
